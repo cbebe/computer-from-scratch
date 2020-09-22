900c8bf4544149b0cee1eee03232cb5cc7f45a91
Charles Ancheta <cancheta@ualberta.ca>
2020-09-21 21:04:06 -0600

    :construction: finish advance method

--- asm/Parser.cpp
+++ asm/Parser.cpp
@@ -24,8 +24,13 @@ void Parser::advance(unsigned long &lineNo) {
     string currentLine;
     bool foundCommand = false;
     while (!foundCommand && std::getline(in, currentLine)) {
-        currentLine = trimWhitespace(currentLine);
         // comments and empty lines are removed
+        currentLine = trimWhitespace(currentLine);
+        unsigned long commentPos = currentLine.find("//");
+        if (commentPos != string::npos) {
+            currentLine.erase(commentPos, currentLine.length() - commentPos);
+        }
+        foundCommand = !currentLine.empty();
     }
     currentCommand = currentLine;
 }
@@ -34,7 +39,7 @@ command Parser::commandType(unsigned long &lineNo) {
     std::regex Argx("@([^0-9][A-Za-z0-9$.:_]*)");
     std::regex Crgx(
         "^([A|D|M]{1,3})? *=? *([01DAM+!&|]*) *;? *(J[MGLE][TEQP])?$");
-    std::regex Lrgx("\(([^0-9][A-Za-z0-9$.:_]*)\)");
+    std::regex Lrgx("\\(([^0-9][A-Za-z0-9$.:_]*)\\)");
     std::vector<std::regex> typeRgx = {Argx, Crgx, Lrgx};
 
     // i is the command type where A = 0, C = 1, L = 2
